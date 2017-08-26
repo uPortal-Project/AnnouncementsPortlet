a784409169bfc78f6c93319521f416c1ac20e4cb
Drew Wills <wills.drew@gmail.com>
2017-08-26 16:28:44 -0700

    ANNPLT-135:  Modernize the RssFeedController

--- src/main/java/org/jasig/portlet/announcements/mvc/servlet/RssFeedController.java
+++ src/main/java/org/jasig/portlet/announcements/mvc/servlet/RssFeedController.java
@@ -6,9 +6,9 @@
  * Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License.  You may obtain a
  * copy of the License at the following location:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -26,116 +26,115 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.feed.synd.SyndFeedImpl;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedOutput;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import javax.portlet.PortletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.jasig.portlet.announcements.model.Announcement;
 import org.jasig.portlet.announcements.model.Topic;
 import org.jasig.portlet.announcements.service.IAnnouncementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/** @author Erik A. Olsson (eolsson@uci.edu) */
+/**
+ * @author Erik A. Olsson (eolsson@uci.edu)
+ */
 @Controller("rssFeedController")
-public class RssFeedController extends AbstractController {
+public class RssFeedController {
 
-  private static final String CONTENT_TYPE = "application/rss+xml";
+    private static final String CONTENT_TYPE = "application/rss+xml";
 
-  private IAnnouncementService announcementService;
+    private IAnnouncementService announcementService;
 
-  @Autowired
-  public void setAnnouncementService(IAnnouncementService announcementService) {
-    this.announcementService = announcementService;
-  }
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-  @Override
-  protected ModelAndView handleRequestInternal(
-      HttpServletRequest request, HttpServletResponse response)
-      throws IllegalStateException, IOException {
-
-    response.setContentType(CONTENT_TYPE);
-
-    Long topicId;
-    try {
-      topicId = Long.valueOf(ServletRequestUtils.getIntParameter(request, "topic"));
-      if (topicId == null) {
-        throw new IllegalStateException("Must specify the topic id");
-      }
-    } catch (Exception e) {
-      response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Must specify topic id");
-      return null;
+    @Autowired
+    public void setAnnouncementService(IAnnouncementService announcementService) {
+        this.announcementService = announcementService;
     }
 
-    Topic t = null;
-    try {
-      t = announcementService.getTopic(topicId);
-    } catch (PortletException e) {
-      e.printStackTrace();
+    @RequestMapping(produces = CONTENT_TYPE)
+    public void getRssFeed(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        // Locate the Topic specified in the request
+        final Long topicId;
+        final Topic topic;
+        try {
+            topicId = Long.valueOf(ServletRequestUtils.getIntParameter(request, "topic"));
+            if (topicId == null) {
+                throw new ServletRequestBindingException("Parameter 'topic' not specified");
+            }
+            try {
+                topic = announcementService.getTopic(topicId);
+            } catch (PortletException pe) {
+                logger.warn("Failed to obtain a Topic for the specified id of '{}'", topicId);
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "no such topic");
+                return;
+            }
+        } catch (ServletRequestBindingException srbe) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Must specify a valid topic id");
+            logger.info("Returning SC_BAD_REQUEST because topicId was improperly specified in the request", srbe);
+            return;
+        }
+
+        // Validate that this topic permits RSS
+        if (!topic.isAllowRss()) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "This topic is not available as RSS");
+            return;
+        }
+
+        // fetch and sort the announcements
+        final List<Announcement> announcements = new ArrayList<>();
+        announcements.addAll(topic.getPublishedAnnouncements());
+        Collections.sort(announcements);
+
+        // create the feed
+        final SyndFeed feed = new SyndFeedImpl();
+        feed.setFeedType("rss_2.0");
+        feed.setTitle(topic.getTitle());
+        feed.setLink(request.getRequestURL().append("?topic=").append(topicId.toString()).toString());
+        feed.setDescription(topic.getDescription());
+
+        final List<SyndEntry> entries = new ArrayList<>();
+        for (Announcement a : announcements) {
+            final SyndEntry entry = new SyndEntryImpl();
+            entry.setTitle(a.getTitle());
+            entry.setAuthor(a.getAuthor());
+            if (a.getLink() != null) entry.setLink(a.getLink());
+            entry.setPublishedDate(a.getStartDisplay());
+            final SyndContent description = new SyndContentImpl();
+            description.setType("text/plain");
+            description.setValue(a.getAbstractText());
+            entry.setDescription(description);
+            entries.add(entry);
+        }
+        feed.setEntries(entries);
+
+        final SyndFeedOutput output = new SyndFeedOutput();
+        final String out;
+        try {
+            out = output.outputString(feed);
+        } catch (FeedException e) {
+            logger.warn("Failed to create SyndFeedOutput for topic '{}'", topic.getTitle());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error generating feed");
+            return;
+        }
+
+        response.setContentLength(out.length());
+        response.getOutputStream().print(out);
+        response.getOutputStream().flush();
     }
 
-    if (t == null) {
-      response.sendError(HttpServletResponse.SC_NOT_FOUND, "no such topic");
-      return null;
-    }
-
-    if (!t.isAllowRss()) {
-      response.sendError(HttpServletResponse.SC_FORBIDDEN, "This topic is not available as RSS");
-      return null;
-    }
-
-    // fetch and sort the announcements
-    List<Announcement> announcements = new ArrayList<Announcement>();
-    announcements.addAll(t.getPublishedAnnouncements());
-
-    Collections.sort(announcements);
-
-    // create the feed
-    SyndFeed feed = new SyndFeedImpl();
-    feed.setFeedType("rss_2.0");
-    feed.setTitle(t.getTitle());
-    feed.setLink(request.getRequestURL().append("?topic=").append(topicId.toString()).toString());
-    feed.setDescription(t.getDescription());
-
-    List<SyndEntry> entries = new ArrayList<SyndEntry>();
-    SyndEntry entry;
-    SyndContent description;
-
-    for (Announcement a : announcements) {
-      entry = new SyndEntryImpl();
-      entry.setTitle(a.getTitle());
-      entry.setAuthor(a.getAuthor());
-      if (a.getLink() != null) entry.setLink(a.getLink());
-      entry.setPublishedDate(a.getStartDisplay());
-      description = new SyndContentImpl();
-      description.setType("text/plain");
-      description.setValue(a.getMessage());
-      entry.setDescription(description);
-      entries.add(entry);
-    }
-
-    feed.setEntries(entries);
-
-    SyndFeedOutput output = new SyndFeedOutput();
-    String out;
-    try {
-      out = output.outputString(feed);
-    } catch (FeedException e) {
-      e.printStackTrace();
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error generating feed");
-      return null;
-    }
-
-    response.setContentLength(out.length());
-    response.getOutputStream().print(out);
-    response.getOutputStream().flush();
-
-    return null;
-  }
 }
